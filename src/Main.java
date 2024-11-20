8b7d4706c49b84a5393eac6205d7693b6dc67e09
Md. Sadman Mubasshir Khan <sadmankhan@iut-dhaka.edu>
2024-11-20 16:12:31 +0600

    Completed main and checked outputs

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,19 @@
 public class Main {
+    public static void main(String[] args) {
+        Order order = new Order(true);
+        order.addFlavor(Flavor.CHOCOLATE_FUDGE);
+        order.addFlavor(Flavor.MINT_CHOCOLATE_CHIP);
+        order.addTopping(Topping.CHOCOLATE_CHIPS);
+        order.addTopping(Topping.FRESH_STRAWBERRIES);
+
+        InvoiceGenerator.generateInvoice(order, "invoice.txt");
+
+        Order order1 = new Order(false);
+        order1.addFlavor(Flavor.PISTACHIO_DELIGHT);
+        order1.addFlavor(Flavor.STRAWBERRY_SWIRL);
+        order1.addTopping(Topping.MARSHMALLOWS);
+        order1.addTopping(Topping.SPRINKLES);
+
+        InvoiceGenerator.generateInvoice(order1, "invoice1.txt");
+    }
 }
