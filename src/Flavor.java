cd35aabdd7dc2253edc11a7427292bf492f6ab31
Md. Sadman Mubasshir Khan <sadmankhan@iut-dhaka.edu>
2024-11-20 15:25:54 +0600

    Completed Flavor and added Topping

--- src/Flavor.java
+++ src/Flavor.java
@@ -1,2 +1,16 @@
 public enum Flavor {
+    MINT_CHOCOLATE_CHIP(2.80),
+    CHOCOLATE_FUDGE(3.00),
+    STRAWBERRY_SWIRL(2.75),
+    PISTACHIO_DELIGHT(3.25);
+
+    private final double pricePerScoop;
+
+    Flavor(double pricePerScoop) {
+        this.pricePerScoop = pricePerScoop;
+    }
+
+    public double getPricePerScoop() {
+        return pricePerScoop;
+    }
 }
