b3e183638692ba1538d430f7dee46ed41e2512d8
Md. Sadman Mubasshir Khan <sadmankhan@iut-dhaka.edu>
2024-11-20 15:26:51 +0600

    Added Topping

---
+++ src/Topping.java
@@ -0,0 +1,16 @@
+public enum Topping {
+    SPRINKLES(0.30),
+    MARSHMALLOWS(0.70),
+    CRUSHED_OREO(0.85),
+    FRESH_STRAWBERRIES(1.00),
+    CHOCOLATE_CHIPS(0.50);
+
+    private final double price;
+    Topping(double price) {
+        this.price = price;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+}
