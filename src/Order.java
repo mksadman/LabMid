d8e197d73af178cc241ab665b0547640276ce7aa
Md. Sadman Mubasshir Khan <sadmankhan@iut-dhaka.edu>
2024-11-20 15:34:13 +0600

    Completed Order

--- src/Order.java
+++ src/Order.java
@@ -1,3 +1,43 @@
-public class Order {
+import java.util.*;
 
+public class Order {
+    private final List<Flavor> flavors = new ArrayList<>();
+    private final List<Topping> toppings = new ArrayList<>();
+    private boolean isWaffleCone;
+    private static final double WAFFLE_CONE_PRICE = 5.00;
+    private static final double TAX_RATE = 0.08;
+
+    public Order(boolean waffleCone) {
+        this.isWaffleCone = waffleCone;
+    }
+
+    public void addFlavor(Flavor flavor) {
+        flavors.add(flavor);
+    }
+
+    public void addTopping(Topping topping) {
+        toppings.add(topping);
+    }
+
+    public double calculateSubtotal() {
+        double subtotal = 0.0;
+        for (Flavor flavor : flavors) {
+            subtotal += flavor.getPricePerScoop();
+        }
+        for (Topping topping : toppings) {
+            subtotal += topping.getPrice();
+        }
+        if (isWaffleCone) {
+            subtotal += WAFFLE_CONE_PRICE;
+        }
+        return subtotal;
+    }
+
+    public double calculateTax() {
+        return calculateSubtotal() * TAX_RATE;
+    }
+
+    public double calculateTotal() {
+        return calculateSubtotal() + calculateTax();
+    }
 }
